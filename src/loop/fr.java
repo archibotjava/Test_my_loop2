8842e1d71850154dab5382165ba0c128b6980303
Archi <archibk32@gmail.com>
2018-05-13 22:08:10 +0300

    Ver1

---
+++ src/loop/fr.java
@@ -0,0 +1,31 @@
+package loop;
+
+import java.io.*;
+import java.util.*;
+
+public class fr {
+// Переменные создаются внутри цикла
+
+    void fr() {
+        // Создадим цикл в цикле. Масло маслянное =Ъ
+
+        for (int i = 0; i <= 5; i++) {
+            System.out.print("-");
+            }
+                for (int j = 0; j <= 5; j++) {
+                    System.out.println("+");
+                    }
+                    for (int k = 0; k <= 5; k++) {
+                        System.out.print("#");
+                        }
+                            for(int l =0; l<=5; l++){
+                                System.out.print("+");
+
+                            }
+        }
+
+    public static void main(String[] args) {
+        fr res = new fr();
+        res.fr();
+    }
+}
