fdaa301675a842bad7971a8bd16f73a1ae20cd9b
Archi <archibk32@gmail.com>
2018-05-15 20:44:15 +0300

    error in thead "main"

--- src/loop/fr.java
+++ src/loop/fr.java
@@ -4,28 +4,17 @@ import java.io.*;
 import java.util.*;
 
 public class fr {
-// Переменные создаются внутри цикла
-
-    void fr() {
-        // Создадим цикл в цикле. Масло маслянное =Ъ
-
-        for (int i = 0; i <= 5; i++) {
-            System.out.print("-");
-            }
-                for (int j = 0; j <= 5; j++) {
-                    System.out.println("+");
-                    }
-                    for (int k = 0; k <= 5; k++) {
-                        System.out.print("#");
-                        }
-                            for(int l =0; l<=5; l++){
-                                System.out.print("+");
-
-                            }
-        }
-
     public static void main(String[] args) {
-        fr res = new fr();
-        res.fr();
+        int n = Integer.parseInt(args[0]);
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if ((i % j == 0) || (j % i == 0)) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print(" ");
+                }
+                System.out.println(i);
+            }
+        }
     }
 }
